ce26f2094fb3c178727c80575a7d1c50243e27e4
Christopher.diaz <>
2019-05-03 11:01:34 -0500

    init push

---
+++ MiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiApp
+//
+//  Created by Christopher.diaz on 4/30/19.
+//  Copyright © 2019 Christopher.diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var pressButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
